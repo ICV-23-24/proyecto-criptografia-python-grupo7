c9da085f1bc9544312be5aa8955e4e24d6395976
lfvalcar <luisfer998845@gmail.com>
2023-11-19 09:07:56 +0100

    Funcionalidad subida de ficheros flask terminada

--- app.py
+++ app.py
@@ -1,60 +1,72 @@
+# Librerías
+# Aplicación
 from datetime import datetime
 from flask import Flask, render_template, request
+# Funciones
 import functions as f
 
 app = Flask(__name__)
 
 
-# Replace the existing home function with the one below
+# Raíz del proyecto
 @app.route("/")
 def home():
     return render_template("home.html")
 
+# Cifrado simétrico
 @app.route("/csimetrico/", methods=['GET','POST'])
 def csimetrico():
     if request.method == 'POST':
-        message = request.form['message']
-        key = request.form['key']
-        mode = request.form['mode']
+        # Obtenemos del html los valores necesarios
+        archivo=request.files['archivo']
+        algoritmo=request.form['algoritmo']
+        almacenamiento=request.form['almacenamiento']
 
-        if mode == 'encrypt':
-            encrypted_message = f.encrypt_message(message, key)
-            return render_template('csimetrico.html', encrypted_message=encrypted_message, mode=mode)
-        elif mode == 'decrypt':
-            decrypted_message = f.decrypt_message(message, key)
-            return render_template('csimetrico.html', decrypted_message=decrypted_message, mode=mode)
-
+        # Guardamos el archivo subido en la aplicación y obtenemos la ruta donde se ubica
+        ruta_archivo=f.subir_archivo(archivo)
+        return render_template("csimetrico.html")
     return render_template("csimetrico.html")
 
+# Cifrado Asimétrico
 @app.route("/casimetrico/")
 def casimetrico():
     return render_template("casimetrico.html")
 
+# Cifrado Híbrido
+@app.route("/chibrido/")
+def chibrido():
+    return render_template("chibrido.html")
 
+# Sobre el equipo
 @app.route("/about/")
 def about():
     return render_template("about.html")
 
+# Documentación del proyecto
 @app.route("/doc/")
 def doc():
     return render_template("doc.html")
 
-@app.route("/otro/")
-def otro():
-    return render_template("otro.html")
+# Listado de los archivos tanto local como compartido
+@app.route("/listadoArchivos")
+def listar_archivos():
+    return render_template("listadoArchivos.html")
 
-
-
-@app.route("/hello/")
-@app.route("/hello/<name>")
-def hello_there(name = None):
-    return render_template(
-        "hello_there.html",
-        name=name,
-        date=datetime.now()
-    )
-
-
+# Datos de la api
 @app.route("/api/data")
 def get_data():
     return app.send_static_file("data.json")
+
+# @app.route("/otro/")
+# def otro():
+#     return render_template("otro.html")
+
+# Página hello
+# @app.route("/hello/")
+# @app.route("/hello/<name>")
+# def hello_there(name = None):
+#     return render_template(
+#         "hello_there.html",
+#         name=name,
+#         date=datetime.now()
+#     )
--- functions.py
+++ functions.py
@@ -1,15 +1,39 @@
+# Librerías
+# Cifrado Simétrico
 from Cryptodome.Cipher import AES
 from Cryptodome.Util.Padding import pad,unpad
 from base64 import b64encode, b64decode
+# Gestión de ficheros Flask
+import os
+from werkzeug.utils import secure_filename
 
+# Variables globales 
+carpeta_ficheros='data/ficheros'
+carpeta_ficheros_encriptados='data/ficheros_encriptados'
+carpeta_claves='data/claves_simetricas'
+
+# Cifrado Simétrico
+# Encriptación AES
 def encrypt_message(message, key):
     key = key.encode('utf-8')
     cipher = AES.new(pad(key, AES.block_size), AES.MODE_ECB)
     encrypted_message = cipher.encrypt(pad(message.encode('utf-8'), AES.block_size))
     return b64encode(encrypted_message).decode('utf-8')
 
+# Desencriptación AES
 def decrypt_message(message, key):
     key = key.encode('utf-8')
     cipher = AES.new(pad(key, AES.block_size), AES.MODE_ECB)
     decrypted_message = unpad(cipher.decrypt(b64decode(message)), AES.block_size).decode('utf-8')
     return decrypted_message
+
+# Gestión de ficheros Flask
+# Subida de archivos
+def subir_archivo(archivo):
+    # Comprobamos con secure_filename de que el archivo no contenga caracteres no seguros.
+    nombre_archivo=secure_filename(archivo.filename)
+    # Almacenamos el archivo en Flask con la librería OS
+    archivo.save(os.path.join(carpeta_ficheros, nombre_archivo))
+    # Obtenemos la ruta del archivo almacenado en string para otras funciones
+    ruta_archivo=carpeta_ficheros+nombre_archivo
+    return ruta_archivo
